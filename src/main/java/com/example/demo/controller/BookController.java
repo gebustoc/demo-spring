2704c4c1d9feffac19244366e69c5c369bd971e0
elarayax <37583319+elarayax@users.noreply.github.com>
2025-10-22 12:26:53 -0300

    Add files via upload

---
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,62 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Book;
+import com.example.demo.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+@Tag(name = "Book Management System")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+    @GetMapping
+    @Operation(summary = "View a list of available books")
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get a book by Id")
+    public Book getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping
+    @Operation(summary = "Add a new book")
+    public Book createBook(@RequestBody Book book) {
+        return bookService.saveBook(book);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update an existing book")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+        Book existingBook = bookService.getBookById(id);
+        if (existingBook != null) {
+            existingBook.setTitle(book.getTitle());
+            existingBook.setAuthor(book.getAuthor());
+            return bookService.saveBook(existingBook);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a book")
+    public void deleteBook(@PathVariable Long id) {
+        
+    }
+}
---
+++ src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,23 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String author;
+}
